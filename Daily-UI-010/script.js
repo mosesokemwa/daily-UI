20f38eda22da10f3ec7dd6d22651a9d0d93a1ba6
mussaimo <okemwamoses@gmail.com>
2016-04-03 20:38:50 +0300

    share button icon animation

--- Daily-UI-010/script.js
+++ Daily-UI-010/script.js
@@ -1,1 +1,18 @@
-
+$('.share-button').on('click', function () {
+    $(this).addClass('open');
+})
+$(".share-items").draggable({
+    axis: "x",
+    containment: ".share-items-wrapper"
+});
+$(".share-item").on('click', function () {
+    $('.share-button').addClass('shared');
+    setTimeout(function () {
+        $('.share-button').addClass('thankyou');
+    }, 800);
+    setTimeout(function () {
+        $('.share-button').removeClass('open');
+        $('.share-button').removeClass('shared');
+        $('.share-button').removeClass('thankyou');
+    }, 2500);
+});
