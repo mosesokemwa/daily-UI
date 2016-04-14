09dd244dc07a7f7d05150dff1c8d5da8231af8e7
mussaimo <okemwamoses@gmail.com>
2016-04-14 18:26:23 +0300

    modified to suite

--- Daily-UI-016/script.js
+++ Daily-UI-016/script.js
@@ -10,71 +10,17 @@ $(function () {
             return '<p>' + $(this.ele).attr('title') + '</p>';
         }
     });
-    // jQuery for content
-    $('.jquery_popup').popup({
-        content: $('#inline')
-    });
-    // HTML for content
-    $('.html_popup').popup({
-        content: '<h1>This is some HTML</h1>',
-        type: 'html'
-    });
     // Custom YouTube content
     $('.youtube_popup').popup({
         types: {
             youtube: function (content, callback) {
-                content = '<iframe width="420" height="315" src="' + content + '" frameborder="0" allowfullscreen></iframe>';
+                content = '<iframe width="800" height="600" src="' + content + '" frameborder="0" allowfullscreen></iframe>';
                 // Don't forget to call the callback!
                 callback.call(this, content);
             }
         },
-        width: 420,
-        height: 315
-    });
-    // Animated popup
-    $('.animated_popup').popup({
-        show: function ($popup, $back) {
-            var plugin = this,
-                center = plugin.getCenter();
-            $popup
-                .css({
-                    top: -$popup.children().outerHeight(),
-                    left: center.left,
-                    opacity: 1
-                })
-                .animate({
-                    top: center.top
-                }, 500, 'easeOutBack', function () {
-                    // Call the open callback
-                    plugin.o.afterOpen.call(plugin);
-                });
-        }
-    });
-    // Call ALL the callbacks
-    $('.callback_popup').popup({
-        beforeOpen: function (type) {
-            console.log('beforeOpen -', type);
-        },
-        afterOpen: function () {
-            console.log('afterOpen');
-        },
-        beforeClose: function () {
-            console.log('beforeClose');
-        },
-        afterClose: function () {
-            console.log('afterClose');
-        }
-    });
-    // Different preloader
-    $('.preloader_popup').popup({
-        preloaderContent: '<img src="assets/images/preloader.gif" class="preloader">'
-    });
-    // Error popup
-    $('.error_popup').popup({
-        error: function (content, type) {
-            // Just call open again, it'll replace the content
-            this.open('<h1>ERROR!</h1><p>Content "' + content + '" of type "' + type + '" could not be loaded.</p>', 'html');
-        }
+        width: 600,
+        height: 400
     });
 });
 /*---------------------
